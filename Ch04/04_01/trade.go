24bfa82f1d60afe91c3413f97baaf6515046290f
RUDRA PRATAP <rs9046496998@gmail.com>
2020-10-14 11:52:08 +0530

    remove extra words

--- Ch04/04_01/trade.go
+++ Ch04/04_01/trade.go
@@ -1,5 +1,5 @@
 // struct demo
-// In go everything starts with starts with Capital letter is accessible outside
+// In go everything starts with Capital letter is accessible outside
 package main
 
 import (
